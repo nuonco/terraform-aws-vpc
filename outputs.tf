c9fe2e9350d842ac76047a5d8800e032f4721886
Dat Boi Diego <fidiego@users.noreply.github.com>
2024-10-23 17:37:56 -0400

    feat: remove stray attribute (#6)

--- outputs.tf
+++ outputs.tf
@@ -15,9 +15,7 @@ output "vpc" {
     public_subnet_cidr_blocks = module.vpc.public_subnets_cidr_blocks
     public_subnet_ids         = module.vpc.public_subnets
 
-    default_security_group_id = aws_security_group.runner.id
-    db_subnet_group_name      = module.vpc.database_subnet_group_name
-    db_subnet_group_id        = module.vpc.database_subnet_group
+    db_subnet_group_name = module.vpc.database_subnet_group_name
+    db_subnet_group_id   = module.vpc.database_subnet_group
   }
-
 }
